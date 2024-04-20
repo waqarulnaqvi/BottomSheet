0ec9011e0e5343f97d3d5e156dcbea319502541b
Nirvana <syedwaqarul786@gmail.com>
2024-04-20 15:47:43 +0530

    Initial commit

---
+++ app/src/main/java/com/mysteriouscoder/bottomsheet/BottomSheetFragment.java
@@ -0,0 +1,69 @@
+package com.mysteriouscoder.bottomsheet;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link BottomSheetFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    private Context context;
+    private int style;
+
+    public BottomSheetFragment(Context context, int style) {
+        this.context = context;
+        this.style = style;
+    }
+
+    public BottomSheetFragment() {
+        // Required empty public constructor
+    }
+
+    public static BottomSheetFragment newInstance(String param1, String param2) {
+        BottomSheetFragment fragment = new BottomSheetFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_bottom_sheet, container, false);
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return new BottomSheetDialog(getContext(), style);
+    }
+}
---
+++ app/src/main/java/com/mysteriouscoder/bottomsheet/MainActivity.java
@@ -0,0 +1,31 @@
+package com.mysteriouscoder.bottomsheet;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        button = findViewById(R.id.bottom_sheet);
+        button.setOnClickListener(v -> {
+            BottomSheetFragment bottomSheetFragment = new BottomSheetFragment(MainActivity.this,R.style.BottomSheetStyle);
+            bottomSheetFragment.show(getSupportFragmentManager(), bottomSheetFragment.getTag());
+        });
+    }
+}
